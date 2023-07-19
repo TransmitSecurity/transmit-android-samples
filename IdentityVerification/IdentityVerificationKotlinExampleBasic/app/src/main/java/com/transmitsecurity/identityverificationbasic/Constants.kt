978dd163e1db4ca583cb15e36f469735b6b076f5
Milina-transmit <99127305+Milina-transmit@users.noreply.github.com>
2023-07-19 15:25:26 +0300

    Delete IdentityVerification directory

--- IdentityVerification/IdentityVerificationKotlinExampleBasic/app/src/main/java/com/transmitsecurity/identityverificationbasic/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.transmitsecurity.identityverificationbasic
-
-class Constants {
-    companion object {
-        const val CLIENT_ID = "client_id"
-        const val CLIENT_SECRET = "client_secret"
-    }
-}
