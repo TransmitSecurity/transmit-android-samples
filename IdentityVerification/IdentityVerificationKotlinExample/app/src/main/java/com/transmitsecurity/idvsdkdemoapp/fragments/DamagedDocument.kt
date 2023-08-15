daf61016612caf7c29185ad24bf1eafcc24cddad
Milina-transmit <99127305+Milina-transmit@users.noreply.github.com>
2023-08-15 16:25:09 +0300

    Delete IdentityVerification directory

--- IdentityVerification/IdentityVerificationKotlinExample/app/src/main/java/com/transmitsecurity/idvsdkdemoapp/fragments/DamagedDocument.kt
+++
@@ -1,8 +0,0 @@
-package com.transmitsecurity.idvsdkdemoapp.fragments
-
-import com.transmitsecurity.idvsdkdemoapp.R
-
-class DamagedDocument: SecondFragment() {
-    override val recaptureReason: String
-        get() = getString(R.string.error_1)
-}
--- IdentityVerification/IdentityVerificationKotlinExample/app/src/main/java/com/transmitsecurity/idvsdkdemoapp/fragments/DocumentUnsupported.kt
+++
@@ -1,9 +0,0 @@
-package com.transmitsecurity.idvsdkdemoapp.fragments
-
-import com.transmitsecurity.idvsdkdemoapp.R
-
-class DocumentUnsupported: SecondFragment() {
-    override val recaptureReason: String
-        get() = getString(R.string.error_3)
-
-}
